514e8604ae221aeaad599dddc77f199a65ece355
chuongite <simane161@gmail.com>
2025-12-01 21:33:07 +0700

    Submit Bai Tap lop animal & interface edible

---
+++ Animal/App.java
@@ -0,0 +1,30 @@
+import src.fruit.Apple;
+import src.fruit.Fruit;
+import src.fruit.Orange;
+import src.animal.Animal;
+import src.animal.Chicken;
+import src.animal.Tiger;
+import src.edible.Edible;
+
+public class App {
+    public static void main(String[] args) {
+        Animal[] animals = new Animal[2];
+        animals[0] = new Tiger();
+        animals[1] = new Chicken();
+        for (Animal animal : animals) {
+            System.out.println(animal.makeSound());
+
+            if (animal instanceof Chicken) {
+                Edible edibler = (Chicken) animal;
+                System.out.println(edibler.howToEat());
+            }
+        }
+
+        Fruit[] fruits = new Fruit[2];
+        fruits[0] = new Orange();
+        fruits[1] = new Apple();
+        for (Fruit fruit : fruits) {
+            System.out.println(fruit.howToEat());
+        }
+    }
+}
